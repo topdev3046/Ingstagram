fef3d816fa20c1a76428de5cc38b1321290fee02
Taejun Park <assistcloud11@gmail.com>
2018-01-05 13:32:09 +0900

    Created the Like model and fixing Comment and Image models
    - Command List
    
    $ python manage.py makemigrations
    
    Migrations for 'images':
      ingstagram/images/migrations/0002_auto_20180105_1330.py
        - Create model Like
        - Add field creator to comment
        - Add field image to comment
        - Add field creator to image
        - Add field image to like
    
    $ python manage.py migrate
    
    Operations to perform:
      Apply all migrations: account, admin, auth, contenttypes, images, sessions, sites, socialaccount, users
    Running migrations:
      Applying images.0002_auto_20180105_1330... OK

--- ingstagram/images/models.py
+++ ingstagram/images/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from ingstagram.users import models as user_models
+
 
 class TimeStampedModel(models.Model):
 
@@ -14,8 +16,17 @@ class Image(TimeStampedModel):
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
 
 
 class Comment(TimeStampedModel):
 
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
+
+
+class Like(TimeStampedModel):
+
+    creator = models.ForeignKey(user_models.User, null=True)
+    image = models.ForeignKey(Image, null=True)
