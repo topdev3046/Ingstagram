f4680b2a55872e886c153431039d4cf9f578b4e3
Taejun Park <assistcloud11@gmail.com>
2018-01-08 18:43:59 +0900

    Commenting on an Image

--- ingstagram/images/views.py
+++ ingstagram/images/views.py
@@ -20,7 +20,6 @@ class Feed(APIView):
 
         sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
         serializer = serializers.ImageSerializer(sorted_list, many=True)
-
         return Response(serializer.data)
 
 
@@ -41,16 +40,46 @@ class LikeImage(APIView):
                 image=found_image
             )
             preexisiting_like.delete()
-
             return Response(status=status.HTTP_204_NO_CONTENT)
 
         except models.Like.DoesNotExist:
-
             new_like = models.Like.objects.create(
                 creator=user,
                 image=found_image
             )
             new_like.save()
-
             return Response(status=status.HTTP_201_CREATED)
 
+
+class CommentOnImage(APIView):
+
+    def post(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = serializers.CommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(creator=user, image=found_image)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Comment(APIView):
+
+    def delete(self, request, comment_id, format=None):
+
+        user = request.user
+
+        try:
+            comment = models.Comment.objects.get(id=comment_id, creator=user)
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
