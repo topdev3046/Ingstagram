8b4485cbfd1ea581af3759b8e5d345cf63df8222
Taejun Park <assistcloud11@gmail.com>
2018-01-08 18:00:17 +0900

    Likeing images, Restricting likes

--- ingstagram/images/views.py
+++ ingstagram/images/views.py
@@ -1,6 +1,6 @@
-"""from django.shortcuts import render"""
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 
@@ -26,7 +26,31 @@ class Feed(APIView):
 
 class LikeImage(APIView):
 
-    def get(self, request, image_id, format=None):
+    def post(self, request, image_id, format=None):
 
-        print(image_id)
-        return Response(status=200)
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            preexisiting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            preexisiting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+            new_like.save()
+
+            return Response(status=status.HTTP_201_CREATED)
+
