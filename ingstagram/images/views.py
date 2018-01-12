de9c96d7b0a55b00e7304087031c4a0c2f502f08
Taejun Park <assistcloud11@gmail.com>
2018-01-12 15:08:17 +0900

    Getting the image likes

--- ingstagram/images/views.py
+++ ingstagram/images/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from ingstagram.users import models as user_models
+from ingstagram.users import serializers as user_serializers
 from ingstagram.notifications import views as notification_views
 
 
@@ -26,6 +28,13 @@ class Feed(APIView):
 
 
 class LikeImage(APIView):
+
+    def get(self, request, image_id, format=None):
+        likes = models.Like.objects.filter(image__id=image_id)
+        like_creators_ids = likes.values('creator_id')
+        users = user_models.User.objects.filter(id__in=like_creators_ids)
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, image_id, format=None):
 
