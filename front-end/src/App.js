8ee9a146697ba58aec6091dae6ed7bc9b57f06f5
Taejun Park <assistcloud11@gmail.com>
2018-01-18 16:39:43 +0900

    Creating the User Reducer, Install Redux, React-Redux

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
         return (
             <div className={styles.App}>
                 <header className={styles.App__header}>
-                    <img src={logo} className={styles.App__logo} alt="logo" />
                     <h1 className={styles.App__title}>Welcome to React</h1>
                 </header>
                 <p className={styles.App__intro}>
