47e1aed41c2c88024f225fe087fd4afd50a96fe1
Taejun Park <assistcloud11@gmail.com>
2018-01-23 15:19:39 +0900

    Setting Up the Redux Store with Multiple Reducers

---
+++ front-end/src/redux/configureStore.js
@@ -0,0 +1,7 @@
+import { combineReducers, createStore } from "redux";
+import users from "redux/modules/users";
+
+const reducer = combineReducers({ users });
+let configureStore = initialState => createStore(reducer);
+
+export default configureStore;
