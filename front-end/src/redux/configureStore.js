eaa05de55294c7e368ed1eee92a40de5be437b06
Taejun Park <assistcloud11@gmail.com>
2018-01-23 19:30:57 +0900

    Installing Redux Dev Tools

--- front-end/src/redux/configureStore.js
+++ front-end/src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import { routerReducer, routerMiddleware } from "react-router-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import createHistory from "history/createBrowserHistory";
-import Reactotron from "ReactotronConfig";
 import thunk from "redux-thunk";
+// import Reactotron from "ReactotronConfig";
 import users from "redux/modules/users";
+// import { i18nState } from "redux-i18n";
 
 const env = process.env.NODE_ENV;
 const history = createHistory();
@@ -16,14 +18,17 @@ if (env === "development") {
 
 const reducer = combineReducers({
   users,
-  routing: routerReducer
+  routing: routerReducer,
+  // i18nState
 });
 
 let store;
 
 if (env === "development") {
-  store = initialState =>
-    Reactotron.createStore(reducer, applyMiddleware(...middlewares));
+  store = initialState => createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middlewares))
+  );
 } else {
   store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 }
