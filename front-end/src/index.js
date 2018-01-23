a9af87c4b2514a550dee82edab0929e2b42ad71b
Taejun Park <assistcloud11@gmail.com>
2018-01-23 15:33:53 +0900

    Changing the NODE_PATH

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "index.css";
+import App from "App";
 import { Provider } from "react-redux";
-import configureStore from "./redux/configureStore";
+import configureStore from "redux/configureStore";
 
 const store = configureStore();
 console.log(store.getState())
