456a708d232b9a46f54426ac798f2550eb1a1278
Taejun Park <assistcloud11@gmail.com>
2018-01-31 17:38:12 +0900

    Login/Signup with Facebook

--- front-end/src/components/LoginForm/presenter.js
+++ front-end/src/components/LoginForm/presenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Ionicon from "react-ionicons";
+import FacebookLogin from "react-facebook-login";
 import formStyles from "shared/formStyles.scss";
 
 const LoginForm = (props, context) => (
@@ -29,10 +30,15 @@ const LoginForm = (props, context) => (
       />
     </form>
     <span className={formStyles.divider}>{context.t("or")}</span>
-    <span className={formStyles.facebookLink}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" />{" "}
-      {context.t("Log in with Facebook")}
-    </span>
+    <FacebookLogin
+      appId="1718196768212364"
+      autoLoad={false}
+      fields="name,email,picture"
+      callback={props.handleFacebookLogin}
+      cssClass={formStyles.facebookLink}
+      icon="fa-facebook-official"
+      textButton={context.t("Log in with Facebook")}
+    />
     <span className={formStyles.forgotLink}>
       {context.t("Forgot password?")}
     </span>
@@ -43,7 +49,8 @@ LoginForm.propTypes = {
   handleInputChange: PropTypes.func.isRequired,
   usernameValue: PropTypes.string.isRequired,
   passwordValue: PropTypes.string.isRequired,
-  handleSubmit: PropTypes.func.isRequired
+  handleSubmit: PropTypes.func.isRequired,
+  handleFacebookLogin: PropTypes.func.isRequired
 };
 
 LoginForm.contextTypes = {
