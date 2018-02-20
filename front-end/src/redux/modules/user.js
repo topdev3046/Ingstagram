081ca329ec0a5ac4ed8db83af4ed60dfce180f70
Taejun Park <assistcloud11@gmail.com>
2018-02-20 21:36:00 +0900

    Setting the token and changin the state

--- front-end/src/redux/modules/user.js
+++ front-end/src/redux/modules/user.js
@@ -2,52 +2,77 @@
 
 // actions
 
+const SAVE_TOKEN = "SAVE_TOKEN";
+
 // action creators
+
+function saveToken(token) {
+  return {
+    type: SAVE_TOKEN,
+    token
+  };
+}
 
 // API actions
 
 function facebookLogin(access_token) {
-    return function(dispatch) {
-      fetch("/users/login/facebook/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          access_token
-        })
+  return function(dispatch) {
+    fetch("/users/login/facebook/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        access_token
       })
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(err => console.log(err));
-    };
-  }
-  
-  // initial state
-  
-  const initialState = {
-    isLoggedIn: localStorage.getItem("jwt") || false
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (json.token) {
+          localStorage.setItem("jwt", json.token);
+          dispatch(saveToken(json.token));
+        }
+      })
+      .catch(err => console.log(err));
   };
-  
-  // reducer
-  
-  function reducer(state = initialState, action) {
-    switch (action.type) {
-      default:
-        return state;
-    }
+}
+
+// initial state
+
+const initialState = {
+  isLoggedIn: localStorage.getItem("jwt") ? true : false
+};
+
+// reducer
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case SAVE_TOKEN:
+      return applySetToken(state, action);
+    default:
+      return state;
   }
-  
-  // reducer functions
-  
-  // exports
-  
-  const actionCreators = {
-    facebookLogin
+}
+
+// reducer functions
+
+function applySetToken(state, action) {
+  const { token } = action;
+  return {
+    ...state,
+    isLoggedIn: true,
+    token: token
   };
-  
-  export { actionCreators };
-  
-  // export reducer by default
-  
-  export default reducer;
+}
+
+// exports
+
+const actionCreators = {
+  facebookLogin
+};
+
+export { actionCreators };
+
+// export reducer by default
+
+export default reducer;
